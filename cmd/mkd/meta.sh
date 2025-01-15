fdfd37786eb0023f6c7cf41d7c068e8d361d41e5
吕海涛 <hi@taoshu.in>
2025-01-15 20:56:09 +0800

    支持从文件自动提取文章信息

--- cmd/mkd/meta.sh
+++ cmd/mkd/meta.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $(uname -s) == "Darwin" ]]; then
-	updated=`date -jf "%s" "$(stat -f "%m" $1)" "+%Y-%m-%dT%H:%M:%SZ"`
-else
-	updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1`
-fi
+title=$(basename $1 .md)
+date=$(stat -c "%w" $1|cut -d' ' -f1)
+updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1`
 
 pandoc -s -p -f markdown --wrap=none \
 	--template $ROOT_DIR/meta.tpl \
+	--metadata=title:$title \
+	--metadata=date:$date \
 	--metadata=updated:$updated \
 	--lua-filter $ROOT_DIR/meta.lua \
 	-o - $1
